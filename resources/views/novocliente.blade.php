dea74e4eb0c1003ef97532f9b4ba4dd58a3feff5
fabioNog <fabio.carmo@computacao.ufla.br>
2020-04-22 21:16:55 -0300

    Adionando blade index e novocliente

---
+++ resources/views/novocliente.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Validate</title>
+
+    <style>
+        body{
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <main role="main">
+        <div class="row">
+            <div class="container col-lg-8 offset-lg-2 col-md-8 offset-md-2">
+                <div class="card border">
+                    <div class="card-header">
+                        <div class="card-title">
+                            Cadastro de cliente
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form action="/cliente" method="POST">
+                            <div class="form-group">
+                                <label for="nome">Nome do Cliente</label>
+                                <input type="text" name="nome" id="nome" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="idade">Idade do Cliente</label>
+                                <input type="number" name="idade" id="idade" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="endereco">Endereço do Cliente</label>
+                                <input type="text" name="endereco" id="endereco" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email do Cliente</label>
+                                <input type="text" name="email" id="email" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
+                            <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
